3063094990d504f9c94811fd385762e42b534422
roboticvedant <vnaik792014@gmail.com>
2025-05-04 14:38:17 +0000

    Deploying to gh-pages-msu from @ roboticvedant/roboticvedant.github.io@10e1604cca4000a9194825c82372a7824468ee92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,21 +18,21 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/~naikveda/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/~naikveda/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Vedant K. Naik is a robotics developer, passionate about Autonomous Vehicle and Sustainibility.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/~naikveda/cv/";
